c6f94e96e08250085ae2d4ba43f1f5c082ff0f6b
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-24 18:25:29 -0300

    update

---
+++ src/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+export interface IButton extends React.HTMLProps<HTMLButtonElement> {}
+const Button: React.FC<IButton> = ({ children, onClick }) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+export default Button;
