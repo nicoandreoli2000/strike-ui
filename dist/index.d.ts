1a8fb1cd5d01861d95e216ec99c936f9348cf9ac
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-24 18:28:32 -0300

    update

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+interface IButton extends React.HTMLProps<HTMLButtonElement> {
+}
+export const Button: React.FC<IButton>;
+
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/module.js
@@ -0,0 +1,18 @@
+import {jsx as $hgUW1$jsx} from "react/jsx-runtime";
+import "react";
+
+
+
+const $44aa9bf139b40f61$var$Button = ({ children: children , onClick: onClick  })=>{
+    return /*#__PURE__*/ $hgUW1$jsx("button", {
+        onClick: onClick,
+        children: children
+    });
+};
+var $44aa9bf139b40f61$export$2e2bcd8739ae039 = $44aa9bf139b40f61$var$Button;
+
+
+
+
+export {$44aa9bf139b40f61$export$2e2bcd8739ae039 as Button};
+//# sourceMappingURL=module.js.map
