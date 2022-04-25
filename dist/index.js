8ae2964e0b57dcdc1b237fedc863d522c9165b5c
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-25 11:17:42 -0300

    update

---
+++ dist/index.js
@@ -0,0 +1,42 @@
+var $8zHUo$reactjsxruntime = require("react/jsx-runtime");
+
+function $parcel$exportWildcard(dest, source) {
+  Object.keys(source).forEach(function(key) {
+    if (key === 'default' || key === '__esModule' || dest.hasOwnProperty(key)) {
+      return;
+    }
+
+    Object.defineProperty(dest, key, {
+      enumerable: true,
+      get: function get() {
+        return source[key];
+      }
+    });
+  });
+
+  return dest;
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+var $32eadc3ce11727ee$exports = {};
+
+$parcel$export($32eadc3ce11727ee$exports, "Button", () => $32eadc3ce11727ee$export$353f5b6fc5456de1);
+
+const $32eadc3ce11727ee$export$353f5b6fc5456de1 = ({ testProp: testProp , children: children , onClick: onClick  })=>{
+    return /*#__PURE__*/ $8zHUo$reactjsxruntime.jsxs("button", {
+        onClick: onClick,
+        children: [
+            "Test prop: ",
+            testProp,
+            " - Children: ",
+            children
+        ]
+    });
+};
+
+
+$parcel$exportWildcard(module.exports, $32eadc3ce11727ee$exports);
+
+
+//# sourceMappingURL=index.js.map
---
+++ dist/types.d.ts
@@ -0,0 +1,7 @@
+import { FC, HTMLProps } from "react";
+export interface IButton extends HTMLProps<HTMLButtonElement> {
+    testProp: string;
+}
+export const Button: FC<IButton>;
+
+//# sourceMappingURL=types.d.ts.map
