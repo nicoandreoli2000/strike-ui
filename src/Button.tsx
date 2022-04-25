a7a21718227857e6f05fb0e7a3601fe53cf2c0ee
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-04-25 11:39:07 -0300

    update

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,12 +3,7 @@ import { ButtonProps, Button as ChakraButton } from "@chakra-ui/react";
 export interface IButton extends ButtonProps {
   testProp: string;
 }
-export const Button: FC<IButton> = ({
-  testProp,
-  children,
-  onClick,
-  ...props
-}) => {
+export const Button: FC<IButton> = ({ testProp, children, ...props }) => {
   return (
     <ChakraButton {...props}>
       Test prop: {testProp} - Children: {children}
